70d8c83c4ecec9a42bbe3501b1b701bdf99ffd8a
Evgeniy <lev-sait@mail.ru>
2019-05-10 10:50:20 +0300

    методы, помеченные аннотацией собираются в конструкторе хэндлера

--- hw4-aop/src/main/java/ru/otus/hw4/ClassObjectCreator.java
+++ hw4-aop/src/main/java/ru/otus/hw4/ClassObjectCreator.java
@@ -1,27 +1,13 @@
 package ru.otus.hw4;
 
 import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ClassObjectCreator {
 
     public static <T, V> T createClassObject(Class<V> clazz) throws Exception {
+        InvocationHandler handler = new MyInvocationHandler(clazz.getConstructor().newInstance());
 
-        Method[] methods = clazz.getMethods();
-        List<Method> markedLogAnnotationMethods = new ArrayList<>();
-
-        for(Method method : methods) {
-            if(method.isAnnotationPresent(Log.class)) {
-                markedLogAnnotationMethods.add(method);
-            }
-        }
-
-        InvocationHandler handler = new MyInvocationHandler(clazz.getConstructor().newInstance(), markedLogAnnotationMethods);
-
-        return (T)Proxy.newProxyInstance(ClassObjectCreator.class.getClassLoader(), clazz.getInterfaces(), handler);
+        return (T) Proxy.newProxyInstance(ClassObjectCreator.class.getClassLoader(), clazz.getInterfaces(), handler);
     }
 }
--- hw4-aop/src/main/java/ru/otus/hw4/MyInvocationHandler.java
+++ hw4-aop/src/main/java/ru/otus/hw4/MyInvocationHandler.java
@@ -3,34 +3,34 @@ package ru.otus.hw4;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class MyInvocationHandler implements InvocationHandler {
 
     private final Object interfaceClass;
-    private final List<Method> markedLogAnnotationMethods;
+    private final HashSet<String> markedLogAnnotationMethodSet;
 
-    MyInvocationHandler(Object interfaceClass, List<Method> markedLogAnnotationMethods) {
+    MyInvocationHandler(Object interfaceClass) {
         this.interfaceClass = interfaceClass;
-        this.markedLogAnnotationMethods = markedLogAnnotationMethods;
+        markedLogAnnotationMethodSet = new HashSet<>();
+
+        Method[] methods = interfaceClass.getClass().getMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Log.class)) {
+                markedLogAnnotationMethodSet.add(method.getName() + Arrays.toString(method.getParameters()));
+            }
+        }
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-
-        //методы совпадающие по названию и количеству параметров
-        List<Method> similarMethodList = markedLogAnnotationMethods.stream()
-                .filter(item -> item.getName().equals(method.getName()))
-                .filter(item -> item.getParameterCount() == method.getParameterCount())
-                .collect(Collectors.toList());
-
-        if(existCurrentMethodInSimilarMethodList(similarMethodList, method)) {
+        if (markedLogAnnotationMethodSet.contains(method.getName() + Arrays.toString(method.getParameters()))) {
             StringBuilder paramsLog = new StringBuilder(method.getName())
                     .append(": ");
 
             Parameter[] parameters = method.getParameters();
-            for(int i = 0; i < parameters.length; i++) {
+            for (int i = 0; i < parameters.length; i++) {
                 paramsLog
                         .append(parameters[i])
                         .append(" ")
@@ -41,28 +41,5 @@ public class MyInvocationHandler implements InvocationHandler {
             System.out.println(paramsLog);
         }
         return method.invoke(interfaceClass, args);
-    }
-
-    private boolean existCurrentMethodInSimilarMethodList(List<Method> similarMethodList, Method method2) {
-        boolean result = false;
-
-        if(!similarMethodList.isEmpty()) {
-            for (Method method : similarMethodList) {
-                result = true;
-                Parameter[] parameters = method.getParameters();
-                for (int i = 0; i < parameters.length; i++) {
-                    if (!parameters[i].getType().equals(method2.getParameters()[i].getType())) {
-                        result = false;
-                        break;
-                    }
-                }
-
-                if(result) {
-                    break;
-                }
-            }
-        }
-
-        return result;
     }
 }
