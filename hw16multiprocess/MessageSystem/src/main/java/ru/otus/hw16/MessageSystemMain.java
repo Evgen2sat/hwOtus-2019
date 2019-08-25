52003f07d55320a5d2a0a681f4759716315da911
Evgeniy <lev-sait@mail.ru>
2019-08-25 12:26:34 +0300

    запуск frontend из команды

--- hw16multiprocess/MessageSystem/src/main/java/ru/otus/hw16/MessageSystemMain.java
+++ hw16multiprocess/MessageSystem/src/main/java/ru/otus/hw16/MessageSystemMain.java
@@ -1,6 +1,37 @@
 package ru.otus.hw16;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class MessageSystemMain {
-    public static void main(String[] args) {
+    private static final String CLIENT_START_COMMAND = "java -jar ../../FrontendService/target/frontend-service.jar";
+
+    public static void main(String[] args) throws Exception {
+        new MessageSystemMain().start();
+    }
+
+    private void start() throws Exception {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+        startClient(executorService);
+
+        executorService.shutdown();
+    }
+
+    private void startClient(ScheduledExecutorService executorService) {
+        executorService.schedule(() -> {
+            try {
+                new ProcessRunnerImpl().start(CLIENT_START_COMMAND);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }, 5, TimeUnit.SECONDS);
+
     }
 }
