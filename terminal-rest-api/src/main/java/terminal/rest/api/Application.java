7056b6cb28ae9512ef3c32265da33593a1daf6b4
Evgeniy <lev-sait@mail.ru>
2019-09-03 12:36:32 +0300

    добавлен проект для проектной работы

---
+++ terminal-rest-api/src/main/java/terminal/rest/api/Application.java
@@ -0,0 +1,10 @@
+package terminal.rest.api;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
