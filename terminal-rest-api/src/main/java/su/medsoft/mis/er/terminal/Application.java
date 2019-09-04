aabbc8bc3e370f7035763c69be031dc071890d19
Evgeniy <lev-sait@mail.ru>
2019-09-04 16:01:27 +0300

    добавлен сервис получения расписания

--- terminal-rest-api/src/main/java/su/medsoft/mis/er/terminal/Application.java
+++ terminal-rest-api/src/main/java/su/medsoft/mis/er/terminal/Application.java
@@ -1,6 +1,10 @@
 package su.medsoft.mis.er.terminal;
 
+import io.micronaut.http.HttpResponse;
 import io.micronaut.runtime.Micronaut;
+import io.reactivex.Single;
+import su.medsoft.mis.er.terminal.dto.ErrorDto;
+import su.medsoft.mis.er.terminal.responseMessages.BaseResponseMessage;
 
 public class Application {
 
@@ -12,5 +16,21 @@ public class Application {
 
     public static String getLoggerName() {
         return LOGGER_NAME;
+    }
+
+    public static <T extends BaseResponseMessage> HttpResponse<Single<T>> getResponse(T responseMessage) {
+        if(responseMessage.getErrorDto() != null) {
+            return HttpResponse.badRequest(Single.just(responseMessage));
+        } else {
+            return HttpResponse.ok(Single.just(responseMessage));
+        }
+    }
+
+    public static ErrorDto getError(int code, String message) {
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setCode(code);
+        errorDto.setMessage(message);
+
+        return errorDto;
     }
 }
